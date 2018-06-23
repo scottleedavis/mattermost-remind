7468c011feb7f3a1345060b1fbf5a9e33ea21022
Scott Lee Davis <scottleedavis@gmail.com>
2018-06-23 11:48:41 -0700

    initial commit

---
+++ src/main/java/scottleedavis/mattermost/remind/mattermostremind/MattermostRemindApplication.java
@@ -0,0 +1,12 @@
+package scottleedavis.mattermost.remind.mattermostremind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MattermostRemindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MattermostRemindApplication.class, args);
+	}
+}

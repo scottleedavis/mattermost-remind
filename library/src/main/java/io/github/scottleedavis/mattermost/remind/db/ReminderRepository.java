d4b0352f38076d06eefdb9c792123bb9d27b8ad9
Scott Davis <scottd@collegenet.com>
2018-07-05 08:14:08 -0700

    add repository annotation

--- library/src/main/java/io/github/scottleedavis/mattermost/remind/db/ReminderRepository.java
+++ library/src/main/java/io/github/scottleedavis/mattermost/remind/db/ReminderRepository.java
@@ -1,9 +1,11 @@
 package io.github.scottleedavis.mattermost.remind.db;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ReminderRepository extends JpaRepository<Reminder, Long> {
     List<Reminder> findByUserName(String userName);
 }

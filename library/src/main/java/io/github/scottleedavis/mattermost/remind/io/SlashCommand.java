1a6385fa807b7a0678897d87f6c4d8c21c4cef44
Scott Davis <scottd@collegenet.com>
2018-07-05 00:29:48 -0700

    specify token

--- library/src/main/java/io/github/scottleedavis/mattermost/remind/io/SlashCommand.java
+++ library/src/main/java/io/github/scottleedavis/mattermost/remind/io/SlashCommand.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SlashCommand {
 
-    @Value("${slashCommandToken}")
+    @Value("${remindSlashCommandToken}")
     private String slashCommandToken;
 
     @Autowired

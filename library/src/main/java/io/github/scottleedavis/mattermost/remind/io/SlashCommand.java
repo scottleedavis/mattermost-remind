193a81a921d57c354234031e9a9b0e4f9aafa897
Scott Davis <scottd@collegenet.com>
2018-07-05 01:20:48 -0700

    updat status

--- library/src/main/java/io/github/scottleedavis/mattermost/remind/io/SlashCommand.java
+++ library/src/main/java/io/github/scottleedavis/mattermost/remind/io/SlashCommand.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController
+@RestController(value = "RemindSlashCommand")
 public class SlashCommand {
 
     @Value("${remind.SlashCommandToken}")

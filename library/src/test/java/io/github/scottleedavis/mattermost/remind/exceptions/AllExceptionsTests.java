89a5acca4abe42596cd51e07bfd9853213bd05c9
Scott Davis <scottd@collegenet.com>
2018-07-15 14:06:28 -0700

    added exceptions test.

---
+++ library/src/test/java/io/github/scottleedavis/mattermost/remind/exceptions/AllExceptionsTests.java
@@ -0,0 +1,43 @@
+package io.github.scottleedavis.mattermost.remind.exceptions;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AllExceptionsTests {
+
+    @Test
+    public void formatterException() {
+        assertNotNull(new FormatterException("foo"));
+    }
+
+    @Test
+    public void occurrenceException() {
+        assertNotNull(new OccurrenceException("foo"));
+    }
+
+    @Test
+    public void parserException() {
+        assertNotNull(new ParserException("foo"));
+    }
+
+    @Test
+    public void reminderException() {
+        assertNotNull(new ReminderException("foo"));
+    }
+
+    @Test
+    public void reminderServiceException() {
+        assertNotNull(new ReminderServiceException("foo"));
+    }
+
+    @Test
+    public void tokenException() {
+        assertNotNull(new TokenException("foo"));
+    }
+
+}

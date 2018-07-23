e693c549e67f749d0424eedded5fde13d0649907
Scott Davis <scottd@collegenet.com>
2018-07-23 09:31:30 -0700

    updated sql server example script

--- scripts/reminders.example.sqlserver.sql
+++ scripts/reminders.example.sqlserver.sql
@@ -1,17 +1,31 @@
-create table reminders
+if object_id('reminders', 'U') is not null
+begin
+    DROP TABLE reminders;
+end
+CREATE TABLE dbo.reminders
 (
-  id int identity primary key,
-  target       nvarchar(100)  not null,
-  user_name    nvarchar(100)  not null,
-  message     nvarchar(1000) not null,
-  completed    date
+    reminder_id integer NOT NULL DEFAULT (next value for reminders_reminder_id_seq),
+    target varchar(40) NOT NULL,           
+    user_name varchar(40) NOT NULL,           
+    message nvarchar(4000) NOT NULL,            
+    completed datetime default getutcdate(),
+    CONSTRAINT reminders_pkey PRIMARY KEY (reminder_id)
 )
 
-create table occurrences
+CREATE TABLE dbo.occurrences
 (
-  id int identity primary key,
-  reminder_id  int not null,
-  occurrence    date,
-  snoozed date,
-  repeat nvarchar(100)
+    occurrence_id integer NOT NULL DEFAULT (next value for occurrences_occurrence_id_seq),
+    reminder_id integer NOT NULL,
+    occurrence datetime,--timestamp(4) with time zone NOT NULL,
+    snoozed datetime,--timestamp(4) with time zone,
+    repeat text,-- COLLATE pg_catalog."default",
+    CONSTRAINT occurrences_pkey PRIMARY KEY (occurrence_id),
+    CONSTRAINT reminder_id FOREIGN KEY (reminder_id)
+        REFERENCES dbo.reminders (reminder_id) --MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE CASCADE
 )
+
+CREATE INDEX fki_reminder_id
+    ON dbo.occurrences 
+    (reminder_id)

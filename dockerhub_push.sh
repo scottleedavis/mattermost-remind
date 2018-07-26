e32a249a10e03818861ce40b7afe2551d3ae4d22
Scott Davis <scottd@collegenet.com>
2018-07-26 16:19:38 -0700

    docker all the things yet again!

--- dockerhub_push.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-./mvnw -DREMIND_WEBHOOK=http://localhost:12345 -DREMIND_SLASH=123467890 install dockerfile:build
-docker tag scottleedavis/mattermost-remind:latest scottleedavis/mattermost-remind
-docker push scottleedavis/mattermost-remind
---
+++ run_package.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+
+export REMIND_SLASH_TOKEN="7q61n4m5mfbebpme4ga5nu1gdh"
+export REMIND_WEBHOOK_URL="http://127.0.0.1:8065/hooks/jqg3bkxbip8hzgsrsnzep5jqyc"
+
+./mvnw package

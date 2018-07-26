537aac34f6a7cf456336c65d84d817a82c4d1be3
Scott Davis <scottd@collegenet.com>
2018-07-26 16:40:07 -0700

    last cleaniup

--- run_dockerhub_push.sh
+++ run_dockerhub_push.sh
@@ -3,8 +3,5 @@
 export REMIND_SLASH_TOKEN="7q61n4m5mfbebpme4ga5nu1gdh"
 export REMIND_WEBHOOK_URL="http://127.0.0.1:8065/hooks/jqg3bkxbip8hzgsrsnzep5jqyc"
 
-#./mvnw install dockerfile:build
 docker tag scottleedavis/mattermost-remind-app:latest scottleedavis/mattermost-remind-app
 docker push scottleedavis/mattermost-remind-app
-
-#./mvnw dockerfile:push

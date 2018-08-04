afc85e3e32bb94bf0440b3e9075c4d35a7043043
Scott Davis <scottd@collegenet.com>
2018-08-03 18:27:03 -0700

    update version

--- run_jar.sh
+++ run_jar.sh
@@ -3,4 +3,4 @@
 export REMIND_SLASH_TOKEN="7q61n4m5mfbebpme4ga5nu1gdh"
 export REMIND_WEBHOOK_URL="http://127.0.0.1:8065/hooks/jqg3bkxbip8hzgsrsnzep5jqyc"
 
-java -jar application/target/mattermost-remind-app-0.0.2.jar
+java -jar application/target/mattermost-remind-app-0.0.3.jar
